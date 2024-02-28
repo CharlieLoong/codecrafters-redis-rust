6fe2cceafcd3bc90edc0e963f12563d130d03e6b
CharlieLoong <31471962+CharlieLoong@users.noreply.github.com>
2024-02-28 19:06:35 +0800

    stage 5

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,69 @@
-// Uncomment this block to pass the first stage
-use std::io::{Read, Write};
-use std::net::TcpListener;
-use std::thread;
+mod resp;
 
-fn main() {
+// Uncomment this block to pass the first stage
+
+use anyhow::{anyhow, Result};
+use resp::Value;
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
+
         match stream {
-            Ok(mut _stream) => {
+            Ok((mut _stream, _)) => {
                 println!("accepted new connection");
                 //handle_stream(&mut _stream);
-                thread::spawn(move || handle_stream(&mut _stream));
+                tokio::spawn(async move { handle_stream(_stream).await });
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("error: {}", e);
             }
         }
     }
 }
 
-fn handle_stream(stream: &mut std::net::TcpStream) {
-    const PONG: &[u8] = "+PONG\r\n".as_bytes();
-    let mut buffer = [0; 1024];
+async fn handle_stream(stream: TcpStream) {
+    let mut handler = resp::RespHandler::new(stream);
 
-    while let Ok(bytes_read) = stream.read(&mut buffer) {
-        println!(
-            "received: {}",
-            String::from_utf8_lossy(&buffer[..bytes_read])
-        );
-        stream.write(PONG).expect("write failed");
+    loop {
+        let value = handler.read_value().await.unwrap();
+
+        let response = if let Some(v) = value {
+            let (command, args) = extract_command(v).unwrap();
+            match command.as_str() {
+                "ping" => Value::SimpleString("PONG".to_string()),
+                "echo" => args.first().unwrap().clone(),
+                _ => panic!("Unknown command"),
+            }
+        } else {
+            break;
+        };
+        handler.write_value(response).await.unwrap();
+    }
+}
+
+fn extract_command(value: Value) -> Result<(String, Vec<Value>)> {
+    match value {
+        Value::Array(a) => Ok((
+            unpack_bulk_str(a.first().unwrap().clone())?,
+            a.into_iter().skip(1).collect(),
+        )),
+        _ => Err(anyhow!("Invalid command format")),
+    }
+}
+
+fn unpack_bulk_str(value: Value) -> Result<String> {
+    match value {
+        Value::BulkString(s) => Ok(s),
+        _ => Err(anyhow!("Expect bulk string")),
     }
 }
